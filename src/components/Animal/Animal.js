3fe62f32330f8a75eec6762b53cf3b8a22d684e2
Jessica Mosby <jessicamosby@mac.com>
2023-01-30 13:36:30 -0500

    added img route to Animals.js, and mapping through on Main.js, but still isn't rendering animals on webpage

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,9 @@
-export default function Animal() {
-  return <></>;
+export default function Animal({ name, says, top, left, type }) {
+  return (
+    <div className="animal" style={{ top, left }}>
+      <img src={require(`${type}.svg`)} />
+      <span className="name">{name}</span>
+      <span>{says}</span>
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Main.css';
+import Animal from '../Animal/Animal.js';
+import { animals } from '../../data.js';
+import background from '../../background.png';
 
 export default function Main() {
-  return <main></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((item) => (
+        <Animal key={item.name} {...item} />
+      ))}
+    </main>
+  );
 }
